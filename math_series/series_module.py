d312df90de3c50c389a855b504699dd84dbd11d1
Yonatan Palagashvili <yonimotek@gmail.com>
2020-11-10 17:07:15 -0800

    all tests passing for lucas and fibonacci functions

--- math_series/series_module.py
+++ math_series/series_module.py
@@ -7,3 +7,13 @@ def fibonacci(n):
     if n > 1:
         answer=fibonacci(n-1)+fibonacci(n-2)
     return answer
+
+def lucas(n):
+    answer=0
+    if n == 0:
+        answer = 2
+    if n == 1:
+        answer =3
+    if n > 1:
+        answer=lucas(n-1)+lucas(n-2)
+    return answer
